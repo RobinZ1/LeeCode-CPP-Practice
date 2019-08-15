279973ad404834aed8cf9118debdc6c45ea41b3b
Luobin Zeng  (Robin Z) <41602694+RobinZ1@users.noreply.github.com>
2019-08-15 10:35:53 -0400

    upto q25

--- LeetCodeAcedBegin.cpp
+++ LeetCodeAcedBegin.cpp
@@ -934,4 +934,131 @@ class Solution
 ]
 输出: 1->1->2->3->4->4->5->6
 
+class Solution
+{
+    public:
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        vector<ListNode*> longList;
+        for(ListNode* start:lists)
+        {
+            ListNode* ptr = start;
+            while(ptr != NULL)
+            {
+                longList.push_back(ptr);
+                ptr = ptr->next;
+            }
+        }
+        std::sort(longList.begin(),longList.end(),[](auto x, auto y){return((x->val)<(y->val));});
+        if(longList.size() == 0) return NULL;
+        for(size_t i = 0; i < longList.size()-1; i++)
+        {
+            longList[i]->next = longList[i+1];
+        }
+        longList[longList.size()-1]->next = NULL;
+        return longList[0];
+    }
+};
+
+
+
+
+
+24. swap nodes in pairs 
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(NULL) {}
+}；
+
+class Solution
+{
+    public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        ListNode* dummy = new ListNode(0);
+        ListNode* pre = dummy;
+        dummy->next = head;
+        ListNode* cur = head;
+
+        while(cur != NULL)
+        {
+            ListNode* next = cur->next;
+            if(next == NULL)
+                break;
+            pre->next = next;
+            cur->next = next->next;
+            next->next = cur;
+
+            pre = cur;
+            cur = cur->next;
+        }
+
+        return dummy->next;
+    }
+
+};
+
+
+
+
+25. reverse-nodes-in-k-group
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+示例 :
+
+给定这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+
+class Solution
+{
+    public:
+        ListNode* reverseKGroup(ListNode* head, int k)
+        {
+            ListNode* dummy = new ListNode(0);
+            dummy->next = head;
+            ListNode* pre = dummy;
+            ListNode* cur = head;
+            ListNode* tail = dummy;
+            while(true)
+            {
+                int count = 0;
+                tail = pre;
+                while(tail != NULL && count < k)
+                {
+                    tail = tail->next;
+                    count++;
+                }
+
+                if(tail==NULL) break;
+
+                while(pre->next != tail)
+                {
+                    cur = pre->next;
+                    pre->next = cur->next;
+
+                    cur->next = tail->next;
+                    tail->next = cur;
+                }
+
+                pre = head;
+                tail = head;
+                head = head->next;
+            }
+        return dummy->next;
+        }
+};
+
+
 
